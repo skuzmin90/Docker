daecd64599e4430950b3d8c76b23521670512ffb
Sergey.A.Kuzmin <archi_89@mail.ru>
2021-08-13 18:42:17 +0300

    Create apache Dockerfile

--- local_registry.sh
+++ local_registry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker run -d -p 5000:5000 --restart=always --name registry registry:2 \
-&& docker build -t localhost:5000/web-nginx:0.1 -t localhost:5000/web-nginx:latest \
-&& docker build -t localhost:5000/web-apache:0.1 -t localhost:5000/web-apache:latest \
+&& docker build -t localhost:5000/web-nginx:0.1 -t localhost:5000/web-nginx:latest ./nginx/. \
+&& docker build -t localhost:5000/web-apache:0.1 -t localhost:5000/web-apache:latest ./apache/. \
 && docker-compose up -d
